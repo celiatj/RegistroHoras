ab5844b1646d2ecd6fa77981e6836c8f6c995298
Celia <celiatoribio95@gmail.com>
2023-03-28 20:20:50 +0200

    no login commit

--- app/src/main/java/com/example/calculadorhoras/FirebaseActivity.java
+++ app/src/main/java/com/example/calculadorhoras/FirebaseActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,6 +33,7 @@ public class FirebaseActivity extends Activity {
     private Button login;
     private FirebaseAuth mAuth;
     private static final String TAG = "EmailPassword";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,14 +57,17 @@ public class FirebaseActivity extends Activity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
             reload();
         }
     }
 
     private void reload() {
     }
-    private void updateUI(FirebaseUser user){}
+
+    private void updateUI(FirebaseUser user) {
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -71,17 +76,17 @@ public class FirebaseActivity extends Activity {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
         registro.setOnClickListener(new View.OnClickListener() {
-           // if(correo.isNotEmpty()){}
-           @Override
-           public void onClick(View view) {
-               nombre.setVisibility(View.VISIBLE);
-               apellido.setVisibility(View.VISIBLE);
+            // if(correo.isNotEmpty()){}
+            @Override
+            public void onClick(View view) {
+                nombre.setVisibility(View.VISIBLE);
+                apellido.setVisibility(View.VISIBLE);
 
 
-               String nom = nombre.getText().toString();
-               String ap = apellido.getText().toString();
-               String corr = correo.getText().toString();
-               String cont = contraseya.getText().toString();
+                String nom = nombre.getText().toString();
+                String ap = apellido.getText().toString();
+                String corr = correo.getText().toString();
+                String cont = contraseya.getText().toString();
 /*
             mAuth = FirebaseAuth.getInstance().createUserWithEmailAndPassword(corr,cont).addnCompleteListener{
                 if(this.isSuccessful){
@@ -89,52 +94,16 @@ public class FirebaseActivity extends Activity {
                 }else{
                     //show alert error
                 }
-*/      Map<String, String> datos = new HashMap<>();
+*/
+                Map<String, String> datos = new HashMap<>();
 
-        datos.put("nombre", nom);
-        datos.put("apellidos", ap);
-        datos.put("correo", corr);
-        datos.put("contraseña", cont);
-        databaseReference.child("usuarios").child("usuario").setValue(datos);
-               // [START create_user_with_email]
-               mAuth.createUserWithEmailAndPassword(corr, cont)
-                       .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                           @Override
-                           public void onComplete(@NonNull Task<AuthResult> task) {
-                               if (task.isSuccessful()) {
-                                   // Sign in success, update UI with the signed-in user's information
-                                   Log.d(TAG, "createUserWithEmail:success");
-                                   FirebaseUser user = mAuth.getCurrentUser();
-                                   updateUI(user);
-                               } else {
-                                   // If sign in fails, display a message to the user.
-                                   Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                   Toast.makeText(FirebaseActivity.this, "Authentication failed.",
-                                           Toast.LENGTH_SHORT).show();
-                                   updateUI(null);
-                               }
+                datos.put("nombre", nom);
+                datos.put("apellidos", ap);
+                datos.put("correo", corr);
+                datos.put("contraseña", cont);
+                databaseReference.child("usuarios").child("usuario").setValue(datos);
 
 
-
-                           ;
-                           // [END create_user_with_email]
-                       };
-           });
-
-        };
-
-        });
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String corr = correo.getText().toString();
-                String cont = contraseya.getText().toString();
-
-
-
-                // [START sign_in_with_email]
                 mAuth.signInWithEmailAndPassword(corr, cont)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
@@ -144,34 +113,37 @@ public class FirebaseActivity extends Activity {
                                     Log.d(TAG, "signInWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
                                     updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(FirebaseActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                    updateUI(null);
+
                                 }
+
                             }
                         });
-                // [END sign_in_with_email]
             }
+        });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String corr = correo.getText().toString();
+                String cont = contraseya.getText().toString();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getIdToken() instead.
+                    String uid = user.getUid();
+                }
+
+            }
         });
     }
-
-        private void sendEmailVerification() {
-            // Send verification email
-            // [START send_email_verification]
-            final FirebaseUser user = mAuth.getCurrentUser();
-            user.sendEmailVerification()
-                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            // Email sent
-                        }
-                    });
-            // [END send_email_verification]
-        }
-
-
-    }
+}
